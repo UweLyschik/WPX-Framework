df5f1aaecf54747168719ad8ff4e933f15b147f0
UweLyschik <uwe.lyschik@gmail.com>
2025-05-09 15:21:40 +0200

    Add files via upload

---
+++ wpx.js
@@ -0,0 +1,18 @@
+// Core
+export {WPX_App}        from './src/internal/app.js';
+export {WPX_Storage}    from './src/internal/storage/storage.js';
+
+// Elements
+export {WPX_Badge}      from './src/elements/badge.js';
+export {WPX_Button}     from './src/elements/button.js';
+export {WPX_Card}       from './src/elements/card.js';
+export {WPX_Checkbox}   from './src/elements/checkbox.js';
+export {WPX_Colors}     from './src/elements/colors.js';
+export {WPX_Icon}       from './src/elements/icon.js';
+export {WPX_Log}        from './src/elements/log.js';
+export {WPX_Menu}       from './src/elements/menu/menu.js';
+export {WPX_MenuItem}   from './src/elements/menu/menu-item.js';
+export {WPX_MenuLabel}  from './src/elements/menu/menu-label.js';
+export {WPX_Submenu}    from './src/elements/menu/submenu.js';
+export {WPX_Switch}     from './src/elements/switch.js';
+export {WPX_Toast}      from './src/elements/toast.js';
